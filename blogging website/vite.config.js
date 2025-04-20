aa82712c4dd32301e45b793a08f40a375cdac5a3
animeshmaiti <a.maiti770@gmail.com>
2025-04-20 13:10:31 +0530

    assets management

--- blogging website/vite.config.js
+++ blogging website/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Get the directory name from `import.meta.url`
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
 })
