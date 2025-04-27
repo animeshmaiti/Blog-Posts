22b145705bc8ebb53b1ed9f5f3bf1f1547090f0a
animeshmaiti <a.maiti770@gmail.com>
2025-04-27 23:09:51 +0530

    useContext

--- blogging website/src/main.jsx
+++ blogging website/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
+import { EditorProvider } from './context/editorContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <EditorProvider>
+          <App />
+        </EditorProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
