379efcca7c679bc25e731de9265386c912a061b5
animeshmaiti <a.maiti770@gmail.com>
2025-04-30 22:40:45 +0530

    get latest and trending blog backend

--- server/controllers/getBlog.js
+++ server/controllers/getBlog.js
@@ -2,7 +2,29 @@ import Blog from "../Schema/Blog.js"
 
 export const getLatestBlogs = async (req, res) => {
     let maxLimit = 5;
-    const blogs = await Blog.find({ draft: false });
-    return res.status(200).json({ blogs });
-        
+    try {
+        const blogs = await Blog.find({ draft: false })
+            .populate('author', 'personal_info.profile_img personal_info.username personal_info.fullname -_id')
+            .sort({ 'publishedAt': -1 })
+            .select('blog_id title desc banner activity tags publishedAt -_id')
+            .limit(maxLimit);
+
+        return res.status(200).json({blogs});
+    } catch (error) {
+        return res.status(500).json({error:error.message});
+    }
+}
+export const getTrendingBlogs = async (req, res) => {
+    let maxLimit = 5;
+    try {
+        const blogs = await Blog.find({ draft: false })
+            .populate('author', 'personal_info.profile_img personal_info.username personal_info.fullname -_id')
+            .sort({ 'activity.total_read': -1,'activity.total_likes':-1,'publishedAt': -1 })
+            .select('blog_id title publishedAt -_id')
+            .limit(maxLimit);
+
+        return res.status(200).json({blogs});
+    } catch (error) {
+        return res.status(500).json({error:error.message});
+    }
 }
--- server/routes/getBlogRoutes.js
+++ server/routes/getBlogRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import protectRoute from '../middleware/protectRoute.js';
-import { getLatestBlogs } from '../controllers/getBlog.js';
+import { getLatestBlogs, getTrendingBlogs } from '../controllers/getBlog.js';
 
 
 const router = express.Router();
 
-router.get('/latest-blogs',getLatestBlogs);
+router.get('/latest-blogs', getLatestBlogs);
+router.get('/trending-blogs', getTrendingBlogs);
 
 export default router;
