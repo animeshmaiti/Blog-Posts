44ee248616e523e70505faf7b55ed4d4f3ce74f1
animeshmaiti <a.maiti770@gmail.com>
2025-05-11 23:21:49 +0530

    removed clg

--- server/controllers/getBlog.js
+++ server/controllers/getBlog.js
@@ -2,7 +2,7 @@ import Blog from "../Schema/Blog.js"
 import User from "../Schema/User.js";
 
 export const getLatestBlogs = async (req, res) => {
-    let{ page } = req.body;
+    let { page } = req.body;
     let maxLimit = 5;
     try {
         const blogs = await Blog.find({ draft: false })
@@ -12,18 +12,18 @@ export const getLatestBlogs = async (req, res) => {
             .skip((page - 1) * maxLimit)
             .limit(maxLimit);
 
-        return res.status(200).json({blogs});
+        return res.status(200).json({ blogs });
     } catch (error) {
-        return res.status(500).json({error:error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
 export const countLatestBlogs = async (req, res) => {
     try {
-        const count= await Blog.countDocuments({ draft: false });
-        return res.status(200).json({totalDocs:count});
+        const count = await Blog.countDocuments({ draft: false });
+        return res.status(200).json({ totalDocs: count });
     } catch (error) {
-        return res.status(500).json({error:error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
@@ -32,30 +32,30 @@ export const getTrendingBlogs = async (req, res) => {
     try {
         const blogs = await Blog.find({ draft: false })
             .populate('author', 'personal_info.profile_img personal_info.username personal_info.fullname -_id')
-            .sort({ 'activity.total_read': -1,'activity.total_likes':-1,'publishedAt': -1 })
+            .sort({ 'activity.total_read': -1, 'activity.total_likes': -1, 'publishedAt': -1 })
             .select('blog_id title publishedAt -_id')
             .limit(maxLimit);
 
-        return res.status(200).json({blogs});
+        return res.status(200).json({ blogs });
     } catch (error) {
-        return res.status(500).json({error:error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
 export const searchBlogs = async (req, res) => {
-    let {tag,query,author,page}=req.body;
+    let { tag, query, author, page } = req.body;
     let findQuery;
-    if(query) {
+    if (query) {
         findQuery = { draft: false, title: new RegExp(query, 'i') };
-    } else if(tag) {
+    } else if (tag) {
         tag = tag.toLowerCase();
         findQuery = { draft: false, tags: tag };
-    }else if(author) {
+    } else if (author) {
         findQuery = { draft: false, author: author };
-    }else{
-        return res.status(400).json({error:"Tag or query or author is required in search blogs"});
+    } else {
+        return res.status(400).json({ error: "Tag or query or author is required in search blogs" });
     }
-    
+
     try {
         const blogs = await Blog.find(findQuery)
             .populate('author', 'personal_info.profile_img personal_info.username personal_info.fullname -_id')
@@ -64,31 +64,31 @@ export const searchBlogs = async (req, res) => {
             .skip((page - 1) * 5)
             .limit(5);
 
-        return res.status(200).json({blogs});
+        return res.status(200).json({ blogs });
     } catch (error) {
-        return res.status(500).json({error:error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
-export const countSearchBlogs=async (req,res)=>{
-    let {tag,author,query}=req.body;
+export const countSearchBlogs = async (req, res) => {
+    let { tag, author, query } = req.body;
     let findQuery;
-    if(query) {
+    if (query) {
         findQuery = { draft: false, title: new RegExp(query, 'i') };
-    } else if(tag) {
+    } else if (tag) {
         tag = tag.toLowerCase();
         findQuery = { draft: false, tags: tag };
-    }else if(author) {
+    } else if (author) {
         findQuery = { draft: false, author: author };
-    }else{
-        return res.status(400).json({error:"Tag or query or author is required in search blogs"});
+    } else {
+        return res.status(400).json({ error: "Tag or query or author is required in search blogs" });
     }
 
     try {
-        const count= await Blog.countDocuments(findQuery);
-        return res.status(200).json({totalDocs:count});
+        const count = await Blog.countDocuments(findQuery);
+        return res.status(200).json({ totalDocs: count });
     } catch (error) {
-        return res.status(500).json({error:error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
@@ -101,15 +101,11 @@ export const getBlogById = async (req, res) => {
         if (!blog) {
             return res.status(404).json({ error: "Blog not found" });
         }
-        User.findOneAndUpdate(
+        await User.findOneAndUpdate(
             { _id: blog.author },
             { $inc: { 'account_info.total_reads': 1 } },
             { new: true }
-        ).then(() => {
-            console.log("User's total reads updated");
-        }).catch((error) => {
-            console.error("Error updating user's total reads:", error);
-        });
+        )
         return res.status(200).json({ blog });
     } catch (error) {
         return res.status(500).json({ error: error.message });
