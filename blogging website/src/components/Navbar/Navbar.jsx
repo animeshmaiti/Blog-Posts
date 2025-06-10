dc52fc0f19e99b5218821f9f22ba11b06001cfc2
animeshmaiti <a.maiti770@gmail.com>
2025-06-10 21:33:54 +0530

    side navbar section

--- blogging website/src/components/Navbar/Navbar.jsx
+++ blogging website/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className='navbar'>
+      <nav className='navbar z-50'>
         <Link to='/' className='flex-none w-10'>
           <img src={logo} alt='logo' className='w-full' />
         </Link>
