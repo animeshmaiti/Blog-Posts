1cb921d48e115a81717d2245abfd6011800536ac
animeshmaiti <a.maiti770@gmail.com>
2025-06-27 22:16:58 +0530

    handle withCredentials

--- blogging website/src/common/filterPaginationData.jsx
+++ blogging website/src/common/filterPaginationData.jsx
@@ -6,15 +6,22 @@ export const filterPaginationData = async ({
     data,
     page,
     countRoute,
-    data_to_send={}
+    data_to_send={},
+    verify=false
 }) => {
     let obj;
+    let headers = {}
+    if(verify){
+        headers={
+            withCredentials: true
+        }
+    }
     if (state != null && !create_new_arr) {
         // console.log(state);
         obj = { ...state, results: [...state.results, ...data], page: page };
     } else {
         try{
-            const response = await axios.post('http://localhost:3000'+countRoute, data_to_send);
+            const response = await axios.post('http://localhost:3000'+countRoute, data_to_send,headers);
             const {totalDocs}=response.data;
             obj={
                 results: data,
